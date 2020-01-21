31b3b46478297e479d56b3b2962f61d944187b94
David Smith <david.smith@weaveability.com>
2020-01-21 10:19:04 +0000

    implement frame styles

--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import Radium from "radium";
+import React from "react";
 
-export default function Frame({ children }) {
-  return <div>{children}</div>
-}
+import styles from "./frame-styles.js";
+
+export default Radium(function Frame({ children }) {
+  return <div style={styles.root}>{children}</div>;
+});
